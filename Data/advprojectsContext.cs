777db846cd7f4959b98d303f24c0b93ce3434c67
ONPRAE <onwarapat@gmail.com>
2024-08-07 15:31:42 +0700

    first commit

---
+++ Data/advprojectsContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pjadv.Models;
+
+namespace pjadv.Data
+{
+    public class advprojectsContext : DbContext
+    {
+        //Constructor
+        public advprojectsContext(DbContextOptions<advprojectsContext> options) : base(options) { }
+
+        public advprojectsContext() { }
+
+        public DbSet<Project> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Mapping + จัดการ relations
+            modelBuilder.Entity<Project>().ToTable("Products");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string server = "localhost";
+            string database = "*****";
+            string uid = "*****";
+            string password = "*****";
+            string connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";PORT=3306;SslMode=Required;";
+            optionsBuilder.UseMySQL(connectionString);
+        }
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace pjadv.Models
+{
+    public class Project
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string productCode { get; set; }
+        public string productName { get; set; }
+        public string productLine { get; set; }
+        public string productScale { get; set; }
+        public string productVendor { get; set; }
+        public string productDescription { get; set; }
+        public int quantityInStock { get; set; }
+        public double buyPrice { get; set; }
+        public double salePrice { get; set; }
+    }
+}
